018c26937aaee9f56ea1b391e0b74adec4024844
nonametofind <awsxdrfvgyhnjik@mailfence.com>
2026-06-12 16:51:45 -0700

    Added comments
    all comments in this program  where added in this commit

--- GreedAlgorithm.c
+++ GreedAlgorithm.c
@@ -10,8 +10,11 @@ int main() {
         working = (double)numerator / denominator;
         printf("%f\n", working);
         printf("%d/%d\n\noutput:\n", numerator,denominator);
+        //this while loop is not properly working realisticaly I want "not equal or more than" so currently it functions as while(1)
         while (working != 0) {
+            //0.000008 is here to accound for floating point errors
             if (working >= (1.0 / i)-0.000008) {
+                //125000 is 0.000008 so it breaks if this is not here
                 if(i>=125000) {
                     break;
                 }
@@ -19,6 +22,7 @@ int main() {
                 working = working - (1.0/i);
             }
             i=i+1;
+            //breaks on buffer underflow
             if (i < 0) {
                 working = 0;
             }

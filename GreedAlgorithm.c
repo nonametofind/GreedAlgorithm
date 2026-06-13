cef964a0477ab5127b95448efd282e84e65708a6
nonametofind <awsxdrfvgyhnjik@mailfence.com>
2026-06-12 18:01:53 -0700

    Comment out debug print statements
    Commented out print statements for debugging.

--- GreedAlgorithm.c
+++ GreedAlgorithm.c
@@ -8,8 +8,8 @@ int main() {
     printf("Enter a fraction (e.g., 3/4 or 1/3): ");
     if (scanf("%d/%d", &numerator, &denominator) == 2) {
         working = (double)numerator / denominator;
-        printf("%f\n", working);
-        printf("%d/%d\n\noutput:\n", numerator,denominator);
+        //printf("%f\n", working);
+        //printf("%d/%d\n\noutput:\n", numerator,denominator);
         //this while loop is not properly working realisticaly I want "not equal or more than" so currently it functions as while(1)
         while (working != 0) {
             //0.000008 is here to accound for floating point errors
